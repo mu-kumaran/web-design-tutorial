a837841785af77ee7c623474f96c991428c89381
Manoj Kumar <mu.kumaran1706@gmail.com>
2024-12-23 18:18:27 +0530

    Codes updation

--- web-projects/test-site/scripts/main.js
+++ web-projects/test-site/scripts/main.js
@@ -1,4 +1,4 @@
-const myImage = document.querySelector("img");
+const myImage = document.querySelector("#lion");
 console.log(myImage);
 myImage.addEventListener('click',()=> {
 	const mySrc = myImage.getAttribute('src');
@@ -9,6 +9,28 @@ myImage.addEventListener('click',()=> {
 	}
 });
 
-const myHeading = document.querySelector("h1");
-console.log(myHeading);
-myHeading.textContent = "I'm a hero";
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName(){
+	const myName = prompt("Please enter your name");
+	if (!myName)  	// this means: If myName has no value, run setUserName()
+    	setUserName();
+    else{
+    	localStorage.setItem("name",myName);
+		console.log(localStorage);
+		myHeading.innerHTML = `Fullstack Lion - ${myName} <br>Code like a King`;
+    }
+}
+
+if(!localStorage.getItem("name"))
+	setUserName();
+else
+{
+	const storedName = localStorage.getItem("name");
+	myHeading.innerHTML = `Fullstack Lion - ${storedName} <br>Code like a King`;
+}
+
+myButton.addEventListener("click",()=>{
+	setUserName();
+});
